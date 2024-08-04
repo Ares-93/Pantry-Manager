c7f0acc5d99065a31a0268325dbc2779a91830a9
Mohammad Haider <arzhdr@gmail.com>
2024-08-04 13:40:45 -0400

    added a stack to store the pantry items and a scroll function

--- app/page.js
+++ app/page.js
@@ -1,4 +1,17 @@
-import { Box } from "@mui/material";
+import { Box, Stack } from "@mui/material";
+
+const items = [
+  "tomato",
+  "garlic",
+  "potato ",
+  "brocolli",
+  "carrot",
+  "tamarind",
+  "onion",
+  "ginger",
+  "chilli",
+  "cucumber",
+];
 
 export default function Home() {
   return (
@@ -9,7 +22,20 @@ export default function Home() {
       justifyContent={"center"}
       alignItems={"center"}
     >
-      Whats up
+      <Stack width="800px" height="400px" spacing={2} overflow={"scroll"}>
+        {items.map((item) => (
+          <Box
+            key={item}
+            bgcolor="black"
+            color="crimson"
+            p={2}
+            textAlign="center"
+            fontWeight={700}
+          >
+            {item}
+          </Box>
+        ))}
+      </Stack>
     </Box>
   );
 }
