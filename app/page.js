cb2036d1a94ca64491d67916da2327453d8afcb4
Mohammad Haider <arzhdr@gmail.com>
2024-08-04 14:03:37 -0400

    added header to the list

--- app/page.js
+++ app/page.js
@@ -23,6 +23,23 @@ export default function Home() {
       alignItems={"center"}
     >
       <Stack width="800px" height="400px" spacing={2} overflow={"scroll"}>
+        <Box
+          position="sticky"
+          top={0}
+          bgcolor="black"
+          p={2}
+          textAlign="center"
+          zIndex={1}
+        >
+          <Typography
+            variant="h4"
+            color="orange"
+            textAlign="center"
+            fontWeight="bold"
+          >
+            Pantry Items
+          </Typography>
+        </Box>
         {items.map((item, index) => (
           <Box key={index} bgcolor="black" p={2} textAlign="center">
             <Typography
